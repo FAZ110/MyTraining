879d3a1bdbb8391404ef83657c8ed3629d6ac882
Jan Dyląg <jan.dylag@gmail.com>
2025-06-02 15:00:46 +0200

    Validator done

--- Validator/script.js
+++ Validator/script.js
@@ -1,11 +1,22 @@
-// const email_input = document.getElementById('email_input');
-// const password_input = document.getElementById('password_input');
-// const submit_btn = document.getElementById('submit_btn');
+const email_input = document.getElementById('email_input');
+const password_input = document.getElementById('password_input');
+const submit_btn = document.getElementById('submit_btn');
 
 
-// submit_btn.addEventListener('click', () => {
+submit_btn.addEventListener('click', () => {
+    const email = email_input.value;
+    const password = password_input.value;
 
-// })
+    if(validateEmail(email) && validatePassword(password)){
+        alert('Login successful');
+    }else if (!validateEmail(email) && !validatePassword(password)){
+        alert('Wrong email and password!!')
+    }else if (!validatePassword(password)){
+        alert('Wrong password!')
+    }else{
+        alert('Wrong email!')
+    }
+})
 
 
 
@@ -15,7 +26,7 @@ function validateEmail(email){
 }
 
 function validatePassword(password){
-    const passwordRegex = /^[a-zA-Z0-9._%$+-]$/;
+    const passwordRegex = /^[a-zA-Z0-9._%$+-]{8,}$/;
     const hasUpperCase = /[A-Z]/.test(password);
 
     return passwordRegex.test(password) && hasUpperCase
