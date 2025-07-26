86c7c5fac9eaa7a49bc0c93a8a3cea500d318a1b
Jan Dyląg <jan.dylag@gmail.com>
2025-07-26 12:53:28 +0200

    restricted text area

---
+++ TextArea/script.js
@@ -0,0 +1,33 @@
+const cnt = document.getElementById('word-counter');
+const txtArea = document.getElementById('txt-area');
+const maxChar = 20;
+
+const countWords = (text) =>{
+    if (text.trim() === '') return 0;
+
+    return text.trim().length;
+}
+
+const updateCnt = () =>{
+    const charCnt = countWords(txtArea.value);
+    cnt.textContent = `${charCnt}/${maxChar}`
+
+    if (charCnt >= maxChar){
+        console.log('reached')
+        txtArea.value = txtArea.value.slice(0, maxChar-1);
+        txtArea.style.border = '2px solid red'
+        txtArea.style.color = 'red'
+        cnt.style.color = 'red'
+        return
+
+    }else{
+        txtArea.style.border = '2px solid black'
+        txtArea.style.color = 'black'
+        cnt.style.color = 'black'
+
+    }
+}
+
+txtArea.addEventListener('input', updateCnt);
+
+updateCnt();
