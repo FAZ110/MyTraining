3db2d765b9676f294919dbbaaf675cc9f6ce90bd
Jan Dyląg <jan.dylag@gmail.com>
2025-07-30 18:20:57 +0200

    tic tac toe done

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -10,18 +10,20 @@ if (turn === 0){
     turnSign.innerHTML = "Turn: O's turn"
 }
 
-const XTab = [];
-const OTab = [];
+let XTab = [];
+let OTab = [];
+
+let Xwins = 0
+let Owins = 0
+let moves = 0
 
 const winCombinations = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-        // Columns
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        // Diagonals
         [0, 4, 8],
         [2, 4, 6]
 ]
@@ -34,22 +36,38 @@ boxes.forEach(box => {
 
         if (currBox.innerHTML === '' && turn % 2 === 0){
             currBox.innerHTML = 'X'
-            console.log(currBox)
+            XTab.push(parseInt(currBox.id))
+
+            if (XTab.length >= 3 && checkWin(XTab)){
+                Xwins++
+                score.innerHTML = Xwins + ':' + Owins 
+                gameRestart()
+
+            }
+            moves++
             turn++
-            // let winner = checkWin(XTab)
             turnSign.innerHTML = "Turn: O's turn"
+
         }else if (currBox.innerHTML === ''){
             currBox.innerHTML = 'O'
+            OTab.push(parseInt(currBox.id))
+            if (OTab.length >= 3 && checkWin(OTab)){
+                Owins++
+                score.innerHTML = Xwins + ':' + Owins 
+                gameRestart()
+
+            }
+            moves++
             turn++
             turnSign.innerHTML = "Turn: X's turn"
-            // let winner = checkWin(OTab)
         }else{
             alert('Click on an empty box!')
         }
 
-        
-        
-        
+        if (moves === 9){
+            gameRestart()
+        }
+    
     })
 
 })
@@ -58,18 +76,28 @@ const checkWin = (tab) => {
     let win = false
 
     winCombinations.forEach(winningTab => {
-        console.log(winningTab)
-        for(let i = 0; i <= tab.length; i++){
-            let matches = 0;
-            for(let j = 0; i <= 2; j++){
+        let matches = 0;
+        for(let i = 0; i <= tab.length-1; i++){
+            
+            for(let j = 0; j <= 2; j++){
+                
                 if (winningTab[j] === tab[i]){
                     matches++
                 }
             }
             if(matches === 3){
-                return true
+                win = true
             }
         }
     })
-    return false
+    return win
+}
+
+const gameRestart = () => {
+    boxes.forEach(box =>{
+        box.innerHTML = ''
+    })
+    XTab = []
+    OTab = []
+    moves = 0
 }
